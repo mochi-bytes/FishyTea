750f79452dee49a6e2baf2ee41a965de5d5b3a9f
carol <carolwong492@gmail.com>
2024-04-02 17:13:22 -1000

    Boba cups get bigger when cat is over them

--- Assets/ShopScreen/Scripts/BobaCupMove.cs
+++ Assets/ShopScreen/Scripts/BobaCupMove.cs
@@ -9,9 +9,12 @@ public class BobaCupMove : MonoBehaviour
 
     public bool parentTop;
 
+    public float scaleFactor = 1.2f; // Factor by which to scale up
+    private Vector3 originalScale; // Original scale of the object
+
     void Start()
     {
-
+        originalScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -28,4 +31,24 @@ public class BobaCupMove : MonoBehaviour
         }
 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if collision occurs with the specific object you want
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Increase the scale
+            transform.localScale *= scaleFactor;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Check if the collision ends with the specific object you want
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Revert the scale to the original scale
+            transform.localScale = originalScale;
+        }
+    }
+
 }
