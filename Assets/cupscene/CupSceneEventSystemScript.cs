618b263578a054dedd1ba583b2a892e8a0bdd02e
KaileeHung <97822968+KaileeHung@users.noreply.github.com>
2024-04-17 15:19:17 -1000

    Implemented score text updating

--- Assets/cupscene/CupSceneEventSystemScript.cs
+++ Assets/cupscene/CupSceneEventSystemScript.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventSystemScript : MonoBehaviour
 {
+
+    public string fishTag = "Fish";
+    public Text textToUpdate;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,12 @@ public class EventSystemScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag(fishTag);
         
+        // Get the count of objects
+        int count = objectsWithTag.Length;
+
+        // Update the text component with the count
+        textToUpdate.text = "" + count;
     }
 }
